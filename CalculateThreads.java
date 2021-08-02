fdb6923ca962421a8b64718d741ce29216fea73c
= <mohammed.fathi.allam@gmail.com>
2021-08-02 10:23:48 +0300

    Домашнее задание 12

--- CalculateThreads.java
+++ CalculateThreads.java
@@ -1,17 +1,17 @@
 public class CalculateThreads implements Runnable {
 
     private final float[] arr;
-    private final int iStart;
+    private final int startThread;
 
-    public CalculateThreads(float[] arr, int iStart) {
+    public CalculateThreads(float[] arr, int startThread) {
         this.arr = arr;
-        this.iStart = iStart;
+        this.startThread = startThread;
     }
 
     @Override
     public void run() {
         for (int i = 0; i < arr.length; i++) {
-            arr[i] = (float) (arr[i] * Math.sin(0.2f + (i + iStart) / 5.0) * Math.cos(0.2f + (i + iStart) / 5.0) * Math.cos(0.4f + (i + iStart) / 2.0));
+            arr[i] = (float) (arr[i] * Math.sin(0.2f + (i + startThread) / 5.0) * Math.cos(0.2f + (i + startThread) / 5.0) * Math.cos(0.4f + (i + startThread) / 2.0));
         }
     }
 
